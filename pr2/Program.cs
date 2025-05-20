f81957387f13e8786137b684f4d044a266fccb49
W1ns0 <kuzya_pasha_run@mail.ru>
2025-05-20 23:57:37 +0500

    Изменил полностью код

--- pr2/Program.cs
+++ pr2/Program.cs
@@ -1,62 +1,34 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using pr2;
-using pr2.Models;
 
-namespace ConsoleApp
+
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main()
+        using (ApplicationContext db = new ApplicationContext())
         {
-            //создаём экземпляр контекста бд для работы с данными
-            using (DbCompanyContext context = new DbCompanyContext())
+
+            var employees = db.Employees
+            .Include(u => u.Position)
+            .ThenInclude(c => c!.Department)
+            .ToList();
+
+            foreach (var e in employees)
             {
-                Console.WriteLine("Загрузка данных с помощью Include():");
-                LoadWithInclude(context);
-
-                Console.WriteLine("\n============================\n");
-
-                Console.WriteLine("Загрузка данных с помощью Load():");
-                LoadWithLoad(context);
+                Console.WriteLine($"{e.name} - {e.Position?.title} - {e.Position?.Department?.name}");
             }
-        }
 
-        //связанные данные (отдел и должность) загружаются сразу вместе с сотрудниками одним запросом.
-        static void LoadWithInclude(DbCompanyContext context)
-        {
-            // Загружаем сотрудников в список employees
-            List<Employee> employees = context.Employees
-                .Include(employee => employee.Department) //загружаем для каждого сотрудника отедл
-                .Include(employee => employee.Position) //загружаем для каждого сотрудника должность
-                .ToList(); //получаем список сотрудников и их связй
 
-            //проходимся по каждому сотруднику
-            foreach (Employee employee in employees)
-            {
-                Console.WriteLine($"-Работник: {employee.Name}");
-                Console.WriteLine($"  -Отдел: {employee.Department?.Name}");
-                Console.WriteLine($"  -Должность: {employee.Position?.Name}");
-                Console.WriteLine();
-            }
-        }
 
-        //сначала загружаются все сотрудники, а затем отдельно загружаются все отделы и должности
-        static void LoadWithLoad(DbCompanyContext context)
-        {
-            // Загружаем всех сотрудников в список employees из бд
-            List<Employee> employees = context.Employees.ToList();
+            /*            var employees = db.Employees.ToList();
 
-            // Загружаем все отделы и должности
-            context.Departments.Load();
-            context.Positions.Load();
+                        foreach (var e in employees)
+                        {
+                            db.Entry(e).Reference(emp => emp.Position).Load();
+                            db.Entry(e.Position!).Reference(pos => pos.Department).Load();
 
-            foreach (Employee employee in employees)
-            {
-                Console.WriteLine($"-Работник: {employee.Name}");
-                Console.WriteLine($"  -Отдел: {employee.Department?.Name}");
-                Console.WriteLine($"  -Должность: {employee.Position?.Name}");
-                Console.WriteLine();
-            }
+                            Console.WriteLine($"{e.name} - {e.Position?.title} - {e.Position?.Department?.name}");
+                        }*/
         }
     }
 }
---
+++ pr2/models.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Department> Departments { get; set; } = null!;
+    public DbSet<Position> Positions { get; set; } = null!;
+    public DbSet<Employee> Employees { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=lb2_Company;Username=postgres;Password=1111;");
+    }
+}
+public class Department
+{
+    public int id { get; set; }//первичный ключ
+    public string? name { get; set; }
+
+    public List<Position> Positions { get; set; }
+}
+public class Position
+{
+    public int id { get; set; }
+    public string? title { get; set; }
+    public int DepartmentId { get; set; }//внешний ключ 
+    public Department? Department { get; set; }  //навигационное свойствo
+    public List<Employee> Employees { get; set; }
+}
+public class Employee
+{
+    public int id { get; set; }
+    public string? name { get; set; }
+    public int PositionId { get; set; }//внешний ключ 
+    public Position? Position { get; set; } //навигационное свойство
+}
